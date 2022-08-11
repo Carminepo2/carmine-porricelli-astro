6702c89a2b7b9907136ee0e01fa5d181278dac5c
Carmine Porricelli <carmineporricelli96@gmail.com>
2022-08-11 11:21:04 +0200

    Icon fix

--- src/components/icons/DownloadIcon.tsx
+++ src/components/icons/DownloadIcon.tsx
@@ -12,8 +12,8 @@ const DownloadIcon: FC<Props> = (props) => {
       {...props}
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-linecap="round"
+        stroke-linejoin="round"
         d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
       />
     </svg>
--- src/components/icons/MailIcon.tsx
+++ src/components/icons/MailIcon.tsx
@@ -12,8 +12,8 @@ const MailIcon: FC<Props> = (props) => {
       {...props}
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-linecap="round"
+        stroke-linejoin="round"
         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
       />
     </svg>
