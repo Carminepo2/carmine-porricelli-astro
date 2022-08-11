ba685ecc0e543cf0b91b00b7826898b84d55842c
Carmine Porricelli <carmineporricelli96@gmail.com>
2022-08-11 16:50:05 +0200

    Added sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import preact from "@astrojs/preact"
 import fonts from "astro-fonts-next"
 
 import astroI18next from "astro-i18next"
-import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,15 +18,6 @@ export default defineConfig({
     preact(),
     fonts({
       url: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
-    }),
-    sitemap({
-      i18n: {
-        defaultLocale: "en",
-        locales: {
-          en: "en-US",
-          it: "it-IT",
-        },
-      },
     }),
     astroI18next({
       baseLanguage: "it",
