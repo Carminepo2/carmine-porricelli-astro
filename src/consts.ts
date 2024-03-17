adda8fcf9e7636714f584bbf5032267816e64df4
Carmine Porricelli <carmineporricelli96@gmail.com>
2024-03-18 00:17:43 +0100

    WIP blog

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export const E_MAIL = 'carmineporricelli96@gmail.it'
+export const UPDATED_CV_DATE = '10/07/2022'
+export const GITHUB_REPO_LINK = 'https://github.com/Carminepo2/carmine-porricelli-astro-preact'
+
+export const GITHUB_LINK = 'https://github.com/Carminepo2'
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/carmine-porricelli-9036361aa/'
---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content'
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    lang: z.enum(['it', 'en']),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional()
+  })
+})
+
+export const collections = { blog }
